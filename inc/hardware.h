f32231286cdb68af6aef53956ab67b1734b5cfa2
amackp <abemaclean@gmail.com>
2021-05-27 11:51:31 -0400

    added files

--- inc/hardware.h
+++ inc/hardware.h
@@ -10,15 +10,29 @@
 
 #include <msp430.h>
 
-#define SWITCH_1_IN     P4IN
-#define SWITCH_1        BIT1
-#define SWITCH_2_IN     P4IN
-#define SWITCH_2        BIT0
+#define SWITCH_1_PORT     4
+#define SWITCH_1_PIN      BIT1
+#define SWITCH_2_PORT     4
+#define SWITCH_2_PIN      BIT0
 
-#define LED_RED_OUT     P4OUT
-#define LED_RED         BIT2
-#define LED_GREEN_OUT   P2OUT
-#define LED_GREEN       BIT7
+#define LED_RED_PORT     4
+#define LED_RED_PIN      BIT2
+#define LED_GREEN_PORT   2
+#define LED_GREEN_PIN    BIT7
+
+#define RFM_IRQ_PORT    2
+#define RFM_IRQ_PIN     BIT3
+#define RFM_MISO_PORT   2
+#define RFM_MISO_PIN    BIT5
+#define RFM_MOSI_PORT   2
+#define RFM_MOSI_PIN    BIT6
+#define RFM_SCK_PORT    2
+#define RFM_SCK_PIN     BIT4
+#define RFM_CSN_PORT    3
+#define RFM_CSN_PIN     BIT1
+#define RFM_CE_PORT     3
+#define RFM_CE_PIN      BIT4
+
 
 
 #endif /* INC_HARDWARE_H_ */
--- main.c
+++ main.c
@@ -14,14 +14,12 @@ haptic_state_t haptic_state;
 led_state_t    led_state;
 
 void main(void) {
-    WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
+    WDT_A_hold(WDT_A_BASE);
 
-    //configure IO
-    P1DIR =
-
-    PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
-                                            // to activate previously configured port settings
-
+    gpio_init();
+    clock_init();
+    PMM_unlockLPM5();
+    RTClock_init(0, INTERVAL_TIME);
 
     for(;;) {
         tapper_sm();
@@ -38,7 +36,7 @@ void tapper_sm(){
         break;
     case TAPPER_STATE_SHUTDOWN_TIMEOUT:
         break;
-    case TAPPER_STATE_SYNC:
+    case TAPPER_STATE_SYNCED:
         break;
     default:
         break;
@@ -68,6 +66,90 @@ void tapper_sm(){
         break;
     }
 }
+
+void gpio_init(){
+    GPIO_setAsOutputPin(
+            2,
+            RFM_CSN_PIN + RFM_CE_PIN + LED_GREEN_PIN
+            );
+
+    GPIO_setAsOutputPin(
+            3,
+            RFM_MOSI_PIN + RFM_SCK_PIN
+            );
+    GPIO_setAsOutputPin(
+            4,
+            LED_RED_PIN
+            );
+
+    GPIO_setAsInputPin(
+            RFM_MISO_PORT,
+            RFM_MISO_PIN
+            );
+
+    GPIO_setAsInputPin(
+            4,
+            SWITCH_1_PIN + SWITCH_2_PIN
+            );
+
+
+    //XTAL
+    P2OUT |= BIT4 + BIT6;
+    GPIO_setAsPeripheralModuleFunctionInputPin(
+        GPIO_PORT_P2,
+        GPIO_PIN0 + GPIO_PIN1,
+        GPIO_PRIMARY_MODULE_FUNCTION
+    );
+
+    //I2C
+    GPIO_setAsPeripheralModuleFunctionInputPin(
+        GPIO_PORT_P1,
+        GPIO_PIN2 + GPIO_PIN3,
+        GPIO_PRIMARY_MODULE_FUNCTION
+    );
+
+
+}
+
+void reset_alarm(rtclock_alarm_t * alarm, uint16_t interval){
+    RTClock_get(&alarm_time);
+    alarm->timestamp = alarm_time + interval;
+    RTClock_setAlarm(alarm);
+}
+
+void clock_init(){
+    CS_initClockSignal(
+        CS_FLLREF,
+        CS_REFOCLK_SELECT,
+        CS_CLOCK_DIVIDER_1
+        );
+
+    CS_initFLLSettle(8000, 244);
+
+    CS_setExternalClockSource(32768);
+
+    CS_initClockSignal(
+        CS_SMCLK,
+        CS_DCOCLKDIV_SELECT,
+        CS_CLOCK_DIVIDER_8
+        );
+    CS_initClockSignal(
+        CS_MCLK,
+        CS_DCOCLKDIV_SELECT,
+        CS_CLOCK_DIVIDER_1
+        );
+
+    CS_turnOnXT1LF(CS_XT1_DRIVE_0);
+
+
+    volatile uint32_t m_freq = CS_getMCLK();
+    volatile uint32_t sm_freq = CS_getSMCLK();
+    volatile uint32_t a_feq = CS_getACLK();
+
+    __delay_cycles(1);
+
+}
+
 
 #if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
 #pragma vector=PORT4_VECTOR
